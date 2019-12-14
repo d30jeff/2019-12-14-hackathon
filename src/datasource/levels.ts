f02b568597a737b04b06b910e6b0c1d694a3c4e0
Deojeff Lai <d30jeff@gmail.com>
2019-12-14 16:33:17 +0800

    Add education level

---
+++ src/datasource/levels.ts
@@ -0,0 +1,51 @@
+export interface Level {
+  value: number;
+  name?: string;
+}
+
+export const levels: Level[] = [
+  {
+    value: 1,
+    name: 'Primary - 1',
+  },
+  {
+    value: 2,
+    name: 'Primary - 2',
+  },
+  {
+    value: 3,
+    name: 'Primary - 3',
+  },
+  {
+    value: 4,
+    name: 'Primary - 4',
+  },
+  {
+    value: 5,
+    name: 'Primary - 5',
+  },
+  {
+    value: 6,
+    name: 'Primary - 6',
+  },
+  {
+    value: 11,
+    name: 'Form - 1',
+  },
+  {
+    value: 12,
+    name: 'Form - 2',
+  },
+  {
+    value: 13,
+    name: 'Form - 3',
+  },
+  {
+    value: 14,
+    name: 'Form - 4',
+  },
+  {
+    value: 15,
+    name: 'Form - 5',
+  },
+];
